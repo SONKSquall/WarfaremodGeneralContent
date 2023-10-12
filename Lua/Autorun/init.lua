0a6edf3bc925e5fb545c4890b03296c71923b3c7
SONKSquall <sonk.squall@gmail.com>
2023-10-11 20:43:55 -0500

    Add welcome message that displays when a player joins. Add two new fields to WR (Version, Name)

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -1,7 +1,10 @@
 WR = {} -- Warfare General Content Pack
 
 WR.Path = ...
+WR.Name = "Warfare (General Content Pack)"
+WR.Version = "0.1.6"
 
 dofile(WR.Path .. "/Lua/Scripts/helperfunctions.lua")
 dofile(WR.Path .. "/Lua/Scripts/Server/weapons.lua")
 dofile(WR.Path .. "/Lua/Scripts/Server/pow.lua")
+dofile(WR.Path .. "/Lua/Scripts/Server/welcomemessage.lua")
