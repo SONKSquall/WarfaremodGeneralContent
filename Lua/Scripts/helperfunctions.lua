1f403ef5527f3234236bf50f84024b6167ac7197
SONKSquall <sonk.squall@gmail.com>
2023-10-20 17:43:07 -0500

    add 3 new helper functions

--- Lua/Scripts/helperfunctions.lua
+++ Lua/Scripts/helperfunctions.lua
@@ -44,3 +44,48 @@ function WR.SpawnInventoryItems(Items, TargetInventory)
         end)
     end
 end
+
+function WR.IsValidPlayer(player)
+    
+    if player == nil then return false end
+
+    if player.InGame and not player.SpectateOnly then
+       return true
+    else
+       return false
+    end
+
+end
+
+function WR.GetPointers(valuetable, pointertable, field)
+    
+    local pointers = {}
+
+    for key,value in pairs(valuetable) do
+        for key,pointer in pairs(pointertable) do
+            if value.field == pointer.field then
+                table.insert(pointers, #pointers+1)
+                break
+            end
+        end
+    end
+
+    return pointers
+
+end
+
+function WR.GetDifference(number1, number2)
+
+    if number1 == number2 then return 0 end
+
+    local result = 0
+
+    if number1 > number2 then
+        result = math.abs(number2 - number1)
+    elseif number2 > number1 then
+        result = math.abs(number1 - number2)
+    end
+
+    return result
+
+end
