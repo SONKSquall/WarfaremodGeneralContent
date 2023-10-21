161aa123b0758a6476e5aa1776f731c4441145a8
SONKSquall <sonk.squall@gmail.com>
2023-10-21 17:51:59 -0500

    Objective item now gets the teams using the defender and attacker tags in the sub editer. Then the script gets the ratio between dead and alive players on the attacker team. Finally if there are no defenders in the area and more than 50% of the alive attacker team present then the round ends.

--- Lua/Scripts/Server/game.lua
+++ Lua/Scripts/Server/game.lua
@@ -22,12 +22,36 @@ end)
 
 Hook.add("WR.gameobjective.xmlhook", "WR.gameobjective", function(effect, deltaTime, item, targets, worldPosition)
 
-    local tags = item.Tags
-    -- gets the winner string via a tag in the item
-    local _, _, winner = string.find(tags, 'winnerifbroken="(%a*)"')
-    WR.Game.roundwinner = winner or "..."
+    if WR.Game.roundend == true then return end
 
-    WR.Game.roundend = true
+    local tags = item.Tags
+    local Teams = {}
+    Teams.attacker = {}
+    Teams.attackerteam = {}
+    Teams.defender = {}
+    -- gets the defender & attacker strings from tags placed in the sub editor
+    -- the first two '_' are dummy vars
+    local _, _, defendertag = string.find(tags, 'defender="(%a*)"')
+    local _, _, attackertag = string.find(tags, 'attacker="(%a*)"')
+
+    -- gets the total number of attackers on the team
+    for key,player in pairs(Client.ClientList) do
+        if player.Character.JobIdentifier == attackertag and not player.Character.IsDead then
+            Teams.attackerteam[#Teams.attackerteam+1] = player
+        end
+    end
+    -- gets the number of players present
+    for key,player in pairs(targets) do
+        if player.JobIdentifier == defendertag and not player.IsDead then
+            Teams.defender[#Teams.defender+1] = player
+        elseif player.JobIdentifier == attackertag and not player.IsDead then
+            Teams.attacker[#Teams.attacker+1] = player
+        end
+    end
+    -- if there is more then 50% of the alive attacker team present and no defender then the round ends with attacker victory
+    if math.abs(#Teams.attacker/#Teams.attackerteam) > 0.5 and #Teams.defender == 0 then
+        WR.Game.roundend = true
+    end
 
 end)
 
