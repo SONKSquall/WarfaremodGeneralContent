5aa67ef8c391bd17f6d725861b53d320c4b91d5a
SONKSquall <sonk.squall@gmail.com>
2023-11-16 18:01:49 -0600

    add cardiacarrest, an affliction that increases over time when unconscious and removes itself when revived

--- Lua/Scripts/Server/oxygen.lua
+++ Lua/Scripts/Server/oxygen.lua
@@ -12,8 +12,18 @@ Hook.Add("afflictionUpdate", "WR.unconsciousbreathing", function(affliction, cha
 
     if canbreathe and characterHealth.character.IsUnconscious then
         affliction.SetStrength(affliction.Strength + (-1 * WR.DeltaTime))
+        -- gives cardiac arrest if not already
+        if not characterHealth.GetAffliction("WR_cardiacarrest", false) then
+            WR.GiveAfflictionCharacter(characterHealth.Character, "WR_cardiacarrest", 1.1)
+        end
     else
         return
     end
+
+end)
+
+Hook.Add("WR.removecardiacarrest.xmlhook", "WR.removecardiacarrest", function(effect, deltaTime, item, character, worldPosition)
+
+    character[1].CharacterHealth.GetAffliction("WR_cardiacarrest", false).SetStrength(0.5)
 
 end)
