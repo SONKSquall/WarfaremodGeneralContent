8f3bddba56e19570d0bfa044242012efe78c81e7
SONKSquall <sonk.squall@gmail.com>
2023-11-20 19:25:08 -0600

    add basic recoil code

--- Lua/Scripts/Server/weapons.lua
+++ Lua/Scripts/Server/weapons.lua
@@ -14,3 +14,45 @@ Hook.Add("baton_attack", "WR_BatonImpact", function(effect, deltaTime, item, tar
         limb.character.CharacterHealth.ApplyDamage(limb, AttackResult, true)
     end
 end)
+
+Hook.Add("WR.gunrecoilgain.xmlhook", "WR.gunrecoilgain", function(effect, deltaTime, item, targets, worldPosition)
+    if item.HasTag("gun") then
+        local id = tostring(item.Prefab.Identifier)
+        local spreadtoadd = WR.Config.WeaponRecoil[id].spreadpershot
+        local maxspread = WR.Config.WeaponRecoil[id].maxspread
+        local rangedweapon = item.GetComponentString("RangedWeapon")
+        rangedweapon.Spread = rangedweapon.Spread + spreadtoadd
+
+        if rangedweapon.Spread > maxspread then
+            rangedweapon.Spread = maxspread
+        end
+    end
+end)
+
+local recoiltimerdelay = 15
+local recoiltimertick = 15
+
+Hook.add("think", "WR.gunrecoillose", function()
+
+    recoiltimertick = recoiltimertick - 1
+
+    if recoiltimertick > 0 then return elseif recoiltimertick <= 0 then recoiltimertick = recoiltimerdelay end
+
+    for key,character in pairs(Character.CharacterList) do
+        for item in character.HeldItems do
+            -- the id for objects in the json config is the identifier of the item in question
+            local id = tostring(item.Prefab.Identifier)
+            if item.HasTag("gun") and WR.Config.WeaponRecoil[id] then
+                local minspread = WR.Config.WeaponRecoil[id].minspread
+                local rangedweapon = item.GetComponentString("RangedWeapon")
+                if rangedweapon.Spread < minspread then
+                    rangedweapon.Spread = minspread
+                end
+                -- only decrease if needed
+                if rangedweapon.Spread > minspread then
+                    rangedweapon.Spread = rangedweapon.Spread - WR.Config.WeaponRecoil[id].spreaddecreasepersecond * (WR.DeltaTime * recoiltimerdelay)
+                end
+            end
+        end
+    end
+end)
