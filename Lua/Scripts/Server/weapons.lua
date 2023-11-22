3de3263cdb94398b3660953c5486fc0ea0b2d3fa
SONKSquall <sonk.squall@gmail.com>
2023-11-22 16:32:04 -0600

    Movement and suppression now affect recoil reduction rates. Add new attribute for weaponrecoil json: movementspreadmodifier

--- Lua/Scripts/Server/weapons.lua
+++ Lua/Scripts/Server/weapons.lua
@@ -19,6 +19,7 @@ local playerrecoil = {}
 
 Hook.Add("WR.gunrecoilgain.xmlhook", "WR.gunrecoilgain", function(effect, deltaTime, item, targets, worldPosition)
     if item.HasTag("gun") then
+        -- failsafe incase theres no value for the recoil yet
         playerrecoil[item.ParentInventory] = playerrecoil[item.ParentInventory] or 1
         local id = tostring(item.Prefab.Identifier)
         local spreadpershot = WR.Config.WeaponRecoil[id].spreadpershot
@@ -44,16 +45,23 @@ Hook.add("think", "WR.gunrecoillose", function()
         for item in character.HeldItems do
             -- the id for objects in the json config is the identifier of the item in question
             local id = tostring(item.Prefab.Identifier)
+            -- if the item has a object in the json then execute       
             if item.HasTag("gun") and WR.Config.WeaponRecoil[id] then
+                local movement = character.CurrentSpeed
+                local movementmodifier = WR.Config.WeaponRecoil[id].movementspreadmodifier
+                local suppression = character.CharacterHealth.GetAfflictionStrengthByIdentifier("WR_suppression", false)
+                -- make sure that this modifier stays below 3
+                local reductionmodifier = math.max(1, math.min(1 + (WR.InvLerp(movement*movementmodifier, 0, 3) + WR.InvLerp(suppression, 5, 15)), 3))
+
                 playerrecoil[character.Inventory] = playerrecoil[character.Inventory] or 1
                 local maxspread = WR.Config.WeaponRecoil[id].maxspread
                 local minspread = WR.Config.WeaponRecoil[id].minspread
                 local spreadloss = WR.Config.WeaponRecoil[id].spreaddecreasepersecond
                 local rangedweapon = item.GetComponentString("RangedWeapon")
 
-                playerrecoil[character.Inventory] = math.max(0, WR.InvLerp(WR.Lerp(playerrecoil[character.Inventory], minspread, maxspread) - spreadloss * WR.DeltaTime, minspread, maxspread))
+                playerrecoil[character.Inventory] = math.max(0, WR.InvLerp(WR.Lerp(playerrecoil[character.Inventory], minspread, maxspread) - (spreadloss*WR.DeltaTime) / reductionmodifier, minspread, maxspread))
 
-                rangedweapon.Spread = math.max(maxspread * playerrecoil[item.ParentInventory], minspread)
+                rangedweapon.Spread = math.max(maxspread * playerrecoil[item.ParentInventory], minspread*reductionmodifier) -- minspread is multiplied here so spread is slightly worse when suppressed or moving
             end
         end
     end
