9bba57574104f49d8fbb561c809d02d025ba2bd7
SONKSquall <sonk.squall@gmail.com>
2023-12-08 12:02:22 -0600

    Minedetector bug fixes: fixed repairing doors within its scan radius, fixed error when theres no landmines on the map. Minedetector changes: no longer have to hold right click to scan, condition slowly returns back to full when not in use

--- Lua/Scripts/Server/tools.lua
+++ Lua/Scripts/Server/tools.lua
@@ -2,6 +2,7 @@ if Game.IsMultiplayer and CLIENT then return end
 
 Hook.Add("WR.minedetector.xmlhook", "WR.minedetector", function(effect, deltaTime, item, targets, worldPosition)
     for value in targets do
+        if not Util.GetItemsById("WR_landmine") then return end -- if theres no landmines then return
         for landmine in Util.GetItemsById("WR_landmine") do
             if value == landmine then
                 local light = value.GetComponentString("LightComponent")
