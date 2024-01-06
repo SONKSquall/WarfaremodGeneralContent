2a077ef6d892d552f2e41fb98c4d700b58dcce90
SONKSquall <sonk.squall@gmail.com>
2024-01-06 14:15:17 -0600

    Updated mod version (0.1.7.0)

--- Lua/Autorun/init.lua
+++ Lua/Autorun/init.lua
@@ -2,7 +2,7 @@ WR = {} -- Warfare General Content Pack
 
 WR.Path = ...
 WR.Name = "Warfare (General Content Pack)"
-WR.Version = "0.1.6.9C"
+WR.Version = "0.1.7.0"
 WR.DeltaTime = 0
 WR.Config = json.parse(File.Read(WR.Path .. "/config.json"))
 
