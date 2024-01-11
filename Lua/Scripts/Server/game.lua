967e3ae99b933c73d95e0a8661c74970add7acf2
SONKSquall <sonk.squall@gmail.com>
2024-01-11 12:45:17 -0600

    small refactor on how the round ends

--- Lua/Scripts/Server/game.lua
+++ Lua/Scripts/Server/game.lua
@@ -1,10 +1,33 @@
 if not Game.IsMultiplayer or (Game.IsMultiplayer and CLIENT) then return end
 
 WR.Game = {}
-WR.Game.roundwinner = "..."
-WR.Game.roundending = false
+WR.Game.winner = nil
+WR.Game.ending = false
 WR.Game.roundtick = 0
-WR.Game.roundtickmax = 0
+WR.Game.roundtickmax = 30*60*60
+
+function WR.Game.endgame()
+    if WR.Game.ending or not Game.RoundStarted then return end
+
+    WR.Game.ending = true
+
+    local winnermessage = WR.Game.getwinner() .. " is the winner!"
+    WR.SendMessageToAllClients(winnermessage,nil)
+    WR.Game.winner = nil
+    for n=1,15 do
+        Timer.Wait(function()
+            WR.SendMessageToAllClients("Round ending in "..15-n.." seconds.",{["type"] = ChatMessageType.Server, ["color"] = Color(255, 0, 0, 255), ["sender"] = "Server"})
+        end,n*1000)
+    end
+
+    Timer.Wait(function()
+        Game.EndGame()
+    end,15*1000)
+end
+
+function WR.Game.getwinner()
+    if WR.Game.winner then return WR.Game.winner else return "Unknown winner" end
+end
 
 Game.AddCommand("setroundlength", "Use to set a custom round length in minutes. (Works for one round)", function(args)
     WR.Game.roundtickmax = tonumber(args[1])*60*60
@@ -12,16 +35,16 @@ Game.AddCommand("setroundlength", "Use to set a custom round length in minutes. 
 end, nil, true)
 
 Game.AddCommand("forceend", "Ends the round with a optional winner.", function(args)
-    WR.Game.roundending = true
-    WR.Game.roundwinner = string.gsub(args[1], "_", " ") or "Unknown winner"
+    if args[1] then WR.Game.winner = string.gsub(args[1], "_", " ") end
+    WR.Game.endgame()
 end, nil, true)
 
 Hook.add("roundStart", "WR.GameStart", function()
 
     WR.Game.roundtick = 0
     if WR.Game.roundtickmax == 0 then WR.Game.roundtickmax = 30*60*60 end
-    WR.Game.roundending = false
-    WR.Game.roundwinner = "..."
+    WR.Game.ending = false
+    WR.Game.winner = nil
 
 end)
 
@@ -30,35 +53,16 @@ Hook.add("think", "WR.GameManager", function()
     if WR.Game.roundtickmax == 0 or not Game.RoundStarted then return end
 
     WR.Game.roundtick = WR.Game.roundtick+1
-    -- checks for round end or if the timer is up
-    if WR.Game.roundending or WR.Game.roundtick >= WR.Game.roundtickmax then
-        WR.Game.roundending = false
-
-        local winnermessage = WR.Game.roundwinner .. " is the winner!"
-        WR.SendMessageToAllClients(winnermessage,nil)
-        WR.Game.roundwinner = "..."
-        for n=1,15 do
-            Timer.Wait(function()
-                WR.SendMessageToAllClients("Round ending in "..15-n.." seconds.",{["type"] = ChatMessageType.Server, ["color"] = Color(150, 150, 150, 255), ["sender"] = "Server"})
-            end,n*1000)
-        end
-
-        Timer.Wait(function()
-            Game.EndGame()
-        end,15*1000)
+    -- ends round if the timer is up
+    if WR.Game.roundtick >= WR.Game.roundtickmax then
+        WR.Game.endgame()
     end
-
-end)
-
-Hook.add("roundEnd", "WR.GameEnd", function()
-
-    WR.Game.roundtickmax = 0
 
 end)
 
 Hook.add("WR.gameobjective.xmlhook", "WR.gameobjective", function(effect, deltaTime, item, targets, worldPosition)
 
-    if WR.Game.roundending == true then return end
+    if WR.Game.ending == true then return end
 
     local tags = item.Tags
     local rect = item.WorldRect
@@ -91,8 +95,8 @@ Hook.add("WR.gameobjective.xmlhook", "WR.gameobjective", function(effect, deltaT
     end
     -- if there is more then 50% of the alive attacker team present and no defender then the round ends with attacker victory
     if math.abs(#Teams.attacker/#Teams.attackerteam) > 0.5 and #Teams.defender == 0 then
-        WR.Game.roundwinner = string.gsub(winnertag, "_", " ") or "Unknown winner"
-        WR.Game.roundending = true
+        WR.Game.winner = string.gsub(winnertag, "_", " ")
+        WR.Game.endgame()
     end
 
 end)
