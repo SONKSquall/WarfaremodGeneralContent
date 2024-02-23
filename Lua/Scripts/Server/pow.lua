96a569c3f680dd4d69acf90ec1654d7ae9a1826b
SONKSquall <sonk.squall@gmail.com>
2024-02-23 17:31:10 -0600

    wip refactor of pow.lua

--- Lua/Scripts/Server/pow.lua
+++ Lua/Scripts/Server/pow.lua
@@ -1,31 +1,17 @@
 if Game.IsMultiplayer and CLIENT then return end
 
-Hook.Add("WR.powhandle.xmlhook", "WR.powhandle", function(effect, deltaTime, item, targets, worldPosition)
-    local frendlyteam = ""
-    -- checks if there is a dupe player in the targets (there are two target types), if so then that player's team is the frendly team
-    for player in targets do
-        local instancesofplayer = 0
-        for otherplayer in targets do
-            if player.name == otherplayer.name then
-                instancesofplayer = instancesofplayer + 1
-            end
-        end
-        if instancesofplayer >= 2 then
-            frendlyteam = player.JobIdentifier
-            break
-        end
-    end
+local function powhandle(targets, frendlyteam)
     -- removes pows and places their items in a footlocker
     for character in targets do
         if WR.IsEnemyPOW(character, frendlyteam) == true then
-            local Footlocker = ItemPrefab.GetItemPrefab("WR_footlocker")
-            local AllItems = character.Inventory.FindAllItems(predicate, false, list)
+            local footlocker = ItemPrefab.GetItemPrefab("WR_footlocker")
+            local allItems = character.Inventory.FindAllItems(nil, false, nil)
             local coin = ItemPrefab.GetItemPrefab("WR_currency")
-            Entity.Spawner.AddItemToSpawnQueue(Footlocker, character.WorldPosition, nil, nil, function(Container)
-                WR.SpawnInventoryItems(AllItems, Container.OwnInventory)
+            Entity.Spawner.AddItemToSpawnQueue(footlocker, character.WorldPosition, nil, nil, function(container)
+                WR.SpawnInventoryItems(allItems, container.OwnInventory)
             end)
             -- spawn coins in the frendlyteam's shop
-            for shop in WR.GetItemsById("WR_strategicexchange") do
+            for shop in Util.GetItemsById("WR_strategicexchange") do
                 local _, _, shopteam = string.find(shop.tags, "team='(.-)'")
                 if shopteam and shopteam == frendlyteam then
                     Entity.Spawner.AddItemToSpawnQueue(coin, shop.OwnInventory, nil, nil, nil)
@@ -36,6 +22,16 @@ Hook.Add("WR.powhandle.xmlhook", "WR.powhandle", function(effect, deltaTime, ite
             Entity.Spawner.AddEntityToRemoveQueue(character)
         end
     end
+end
+
+local frendlyteam = ""
+
+Hook.Add("WR.powteamgrabber.xmlhook", "WR.teamgrabber", function(effect, deltaTime, item, targets, worldPosition)
+    frendlyteam = targets[1].JobIdentifier
+end)
+
+Hook.Add("WR.powhandle.xmlhook", "WR.powhandle", function(effect, deltaTime, item, targets, worldPosition)
+    Timer.NextFrame(function() powhandle(targets, frendlyteam) end)
 end)
 
 Hook.Add("WR.cuffs.xmlhook", "WR.cuffs", function(effect, deltaTime, item, targets, worldPosition)
