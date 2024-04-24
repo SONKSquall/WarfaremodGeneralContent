295f4f96627d7c0a94b0fb944f53a699feef16c4
SONKSquall <sonk.squall@gmail.com>
2024-04-24 17:43:25 -0500

    production prototyping

--- Lua/WR/Scripts/Server/items.lua
+++ Lua/WR/Scripts/Server/items.lua
@@ -3,6 +3,7 @@ if Game.IsMultiplayer and CLIENT then return end
 WR.pointItemFunctions = {}
 WR.useItemFunctions = {}
 WR.equipItemFunctions = {}
+WR.spawnItemFunctions = {}
 
 function WR.pointItemFunctions.WR_minedetector(item, itemUser)
 
@@ -55,5 +56,27 @@ end)
 Hook.Add("item.equip", "WR.equip", function(item, itemUser)
     if WR.equipItemFunctions[item.Prefab.Identifier.value] then
         WR.equipItemFunctions[item.Prefab.Identifier.value](item, itemUser)
+    end
+end)
+
+Hook.Add("item.created", "WR.itemSpawn", function(item)
+    if WR.spawnItemFunctions[item.Prefab.Identifier.value] then
+        WR.spawnItemFunctions[item.Prefab.Identifier.value](item)
+    end
+end)
+
+Hook.Add("WR.productionCrate.xmlhook", "WR.productionCrate", function(effect, deltaTime, item, targets, worldPosition)
+    local tagVars = WR.getStringVariables(item.tags)
+    if not (tagVars.spawncount and tagVars.spawnid) then return end
+    local itemSpawned = false
+    if item.OwnInventory.IsEmpty() and item.Condition >= 1 then
+        for i=tonumber(tagVars.spawncount),1,-1 do
+            Entity.Spawner.AddItemToSpawnQueue(ItemPrefab.GetItemPrefab(tagVars.spawnid), item.OwnInventory, nil, nil, function(worldItem) end)
+        end
+        itemSpawned = true
+        item.Condition = item.Condition - 1
+    end
+    if not itemSpawned and item.OwnInventory.IsEmpty() and item.Condition == 0 then
+        Entity.Spawner.AddEntityToRemoveQueue(item)
     end
 end)
