16cb33da12deaab0dfd6210ff313163a0af0a848
SONKSquall <sonk.squall@gmail.com>
2024-05-23 20:18:37 -0500

    Buildings with 3 or more defenses now spawn loot every 30 seconds and can't be destroyed.

--- Lua/WR/Scripts/Server/Extensions/buildingmanager.lua
+++ Lua/WR/Scripts/Server/Extensions/buildingmanager.lua
@@ -42,9 +42,41 @@ local destructionPresets = {
     }
 }
 local building = {}
+building.rect = Vector2(0,0)
 building.walls = {}
+building.containers = {}
+
+building.fortified = false
 building.destroyed = false
 building.destructionPreset = destructionPresets.small
+
+function building:GetRectClients()
+    local clients = {}
+    for client in Client.ClientList do
+        if client.Character and (math.abs(client.Character.WorldPosition.X - self.rect.X - self.rect.Width/2) <= self.rect.Width/2 and math.abs(client.Character.WorldPosition.Y - self.rect.Y + self.rect.Height/2) <= self.rect.Height/2) then
+            table.insert(clients,client)
+        end
+    end
+    return clients
+end
+
+function building:GetRectItems(id,isTag)
+    local items
+    if isTag then
+        items = WR.GetItemsByTag(id)
+    else
+        items = Util.GetItemsById(id)
+    end
+
+    local rectItems = {}
+    for item in items do
+        if (math.abs(item.WorldPosition.X - self.rect.X - self.rect.Width/2) <= self.rect.Width/2 and math.abs(item.WorldPosition.Y - self.rect.Y + self.rect.Height/2) <= self.rect.Height/2) then
+            table.insert(rectItems,item)
+        end
+    end
+    return rectItems
+end
+
 
 function building:GetDamage()
     local damage = 0
@@ -68,7 +100,7 @@ end
 
 function building:IsDestroyed()
     local damageRatio = (self:GetDamage()/self:GetHealth())
-    if damageRatio > self.destructionPreset.threshold then
+    if damageRatio > self.destructionPreset.threshold and not self.fortified then
         return true
     end
     return false
@@ -104,6 +136,22 @@ function building:AddDamage(number)
     end
 end
 
+function building:SpawnItemContainer(index,itemID,amount)
+    index = math.ceil(index)
+    if not self.containers[index] then return end
+    for i=1,amount do
+        Entity.Spawner.AddItemToSpawnQueue(ItemPrefab.GetItemPrefab(tostring(itemID)), self.containers[index].OwnInventory, nil, nil, nil)
+    end
+end
+
+function building:IsFortified()
+    local defenseCount = #self:GetRectItems("WR_spamable",true)
+    if defenseCount >= 3 then
+        return true
+    end
+    return false
+end
+
 function building:new(o)
     o = o or {}   -- create object if user does not provide one
     setmetatable(o, self)
@@ -114,23 +162,39 @@ end
 local buildingManager = class(base)
 buildingManager.name = "Building manager"
 buildingManager.buildings = {}
+-- used for items spawning in intact buildings
+buildingManager.lootTable = {
+    {id = "WR_basicmaterial", amount = 1},
+    {id = "antibleeding1", amount = 3},
+    {id = "antibloodloss1", amount = 2},
+    {id = "WR_sandbag_setup", amount = 1},
+    {id = "WR_barbedwire_setup", amount = 1}
+}
 function buildingManager:onStart()
     -- registers buildings
     for item in Util.GetItemsById("label") do
         if item.HasTag("wr_building") then
-            local areaWalls = {}
+            local walls = {}
+            local containers = {}
             local rect = item.WorldRect
             for wall in Structure.WallList do
                 if wall.MaxHealth < 1000 and not wall.IsPlatform then
                     if (math.abs(wall.WorldPosition.X - rect.X - rect.Width/2) <= rect.Width/2 and math.abs(wall.WorldPosition.Y - rect.Y + rect.Height/2) <= rect.Height/2) then
-                        table.insert(areaWalls,wall)
+                        table.insert(walls,wall)
                     end
                 end
             end
-            print(destructionPresets.small)
+            for container in WR.GetItemsByTag("container") do
+                if (math.abs(container.WorldPosition.X - rect.X - rect.Width/2) <= rect.Width/2 and math.abs(container.WorldPosition.Y - rect.Y + rect.Height/2) <= rect.Height/2) then
+                    table.insert(containers,container)
+                end
+            end
+
             -- each building is its own object
             table.insert(self.buildings, building:new{
-                walls = areaWalls,
+                walls = walls,
+                containers = containers,
+                rect = rect,
                 destructionPreset = destructionPresets[WR.getStringVariables(item.Tags).size]
             })
         end
@@ -139,11 +203,19 @@ end
 function buildingManager:Think()
     if self.enabled then
         self.tick = self.tick + 1
-        if self.tick % 60 == 0 then
-            for obj in self.buildings do
+        for obj in self.buildings do
+            if self.tick % 60 == 0 then
                 if obj.destroyed or obj:IsDestroyed() then
                     obj.destroyed = true
                     obj:Destroy(obj.destructionPreset.dps,obj.destructionPreset.duration)
+                else
+                    obj.fortified = obj:IsFortified()
+                end
+            end
+            if (self.tick % (30*60) == 0) then
+                if obj.fortified and not obj.destroyed then
+                    local loot = self.lootTable[math.random(#self.lootTable)]
+                    obj:SpawnItemContainer(math.random(1,#obj.containers),loot.id,loot.amount)
                 end
             end
         end
