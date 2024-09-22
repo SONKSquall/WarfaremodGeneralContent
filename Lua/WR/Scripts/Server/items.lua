02dd2c32e84b3f23a063e63b6009c13f87ab385f
SONKSquall <sonk.squall@gmail.com>
2024-09-22 15:36:31 -0500

    reduce chance of helmets being blown off

--- Lua/WR/Scripts/Server/items.lua
+++ Lua/WR/Scripts/Server/items.lua
@@ -44,7 +44,7 @@ function WR.characterDamageFunctions.helmet(charHealth, attackResult, hitLimb)
     if not item.HasTag("helmet") then return end
 
     local damage = attackResult.Damage
-    if damage > 25 and math.random() < 0.2 then
+    if damage > 25 and math.random() < 0.05 then
 
         for affliction in attackResult.Afflictions do
             Timer.NextFrame(function()
