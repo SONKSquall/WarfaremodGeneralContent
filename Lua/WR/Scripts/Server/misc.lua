1d1648423a2836b0fca737eaa8da6b1b69a9229b
SONKSquall <sonk.squall@gmail.com>
2024-10-25 19:48:18 -0500

    add night time

--- Lua/WR/Scripts/Server/misc.lua
+++ Lua/WR/Scripts/Server/misc.lua
@@ -47,3 +47,75 @@ end)
 Hook.add("roundStart", "WR.ResetUniquegifts", function()
     uniquegifts = {"nuclearshell","exosuit","WR_germanshoulderstrap","WR_flyboyboots","autoshotgun","assaultrifle","thermalgoggles","nucleargun","radiojammer"}
 end)
+
+
+-------------
+--Halloween--
+-------------
+
+if not WR.HalloweenMode then return end
+
+local periods = {
+    Color(232, 186, 67, 255), -- evening
+    Color(239, 129, 12, 180), -- dusk
+    Color(3, 55, 82, 90), -- twilight
+    Color(0, 26, 38, 10) -- night
+}
+
+-- 15 minutes - 60 secs per minute - 60 ticks per sec - 15 * 60 * 60 = 54000
+local periodInterval = 54000/#periods
+local currentPeriod = 1
+local currentLightIndex = 1
+local skyLights = {}
+
+function WR.roundStartFunctions.dayReset()
+    skyLights = {}
+    currentPeriod = 1
+    currentLightIndex = 1
+
+    for item in Submarine.MainSub.GetItems(true) do
+
+		if item.HasTag("skylight") then
+			table.insert(skyLights, item)
+
+			local lightComp = item.GetComponentString("LightComponent")
+			lightComp.LightColor = periods[1]
+
+			if SERVER then
+			    local prop = lightComp.SerializableProperties[Identifier("LightColor")]
+			    Networking.CreateEntityEvent(item, Item.ChangePropertyEventData(prop, lightComp))
+			end
+
+		end
+	end
+end
+
+function WR.thinkFunctions.dayCycle()
+
+    if not Game.RoundStarted then return end
+    if #skyLights <= 0 then return end
+    if currentPeriod >= #periods then return end
+
+    if WR.tick % 2 == 0 then
+
+        currentPeriod = math.min(math.ceil(WR.tick / periodInterval),#periods)
+
+        if currentPeriod >= #periods then return end
+
+        local light = skyLights[currentLightIndex]
+        local lightComp = light.GetComponentString("LightComponent")
+
+        local ratio = (WR.tick % periodInterval) / periodInterval
+        local color = Color.Lerp(periods[currentPeriod], periods[currentPeriod+1], ratio)
+
+        lightComp.LightColor = color
+
+        if SERVER then
+            local prop = lightComp.SerializableProperties[Identifier("LightColor")]
+            Networking.CreateEntityEvent(light, Item.ChangePropertyEventData(prop, lightComp))
+        end
+
+        currentLightIndex = (currentLightIndex % #skyLights) + 1
+
+    end
+end
