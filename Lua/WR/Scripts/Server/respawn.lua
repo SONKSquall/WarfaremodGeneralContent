c5ba12a79ef8b7611c5df9de597d32c4d5e1d2e5
SONKSquall <sonk.squall@gmail.com>
2024-11-21 23:59:42 -0600

    add message 15 secs before respawn

--- Lua/WR/Scripts/Server/respawn.lua
+++ Lua/WR/Scripts/Server/respawn.lua
@@ -2,20 +2,25 @@ if CLIENT then return end
 
 WR.teamSpawnBlackList = {}
 
-WR.defaultRespawnInterval = 60
-WR.respawnTime = WR.defaultRespawnInterval -- time till respawn in seconds
+WR.defaultRespawnInterval = 120*60
+WR.respawnTime = WR.defaultRespawnInterval -- time till respawn in ticks
 
 WR.respawnEnabled = true
 
 function WR.thinkFunctions.respawn()
 
-    WR.respawnTime = WR.respawnTime - 1 * WR.DeltaTime
+    WR.respawnTime = WR.respawnTime - 1
+
+    if WR.respawnTime == 900 then -- 15 seconds before respawn
+        WR.SendMessageToAllClients("15 seconds before respawn.",{type = ChatMessageType.Default})
+    end
 
     if WR.respawnTime > 0 or not WR.respawnEnabled then return end
 
-    WR.respawnTime = WR.defaultRespawnInterval * 1
+    WR.respawnTime = WR.defaultRespawnInterval * 1 -- planning on changing respawn time depending on battle conditions
 
     WR.AssignBalanceJobs()
+    WR.SendMessageToAllClients("Respawning...",{type = ChatMessageType.Default})
     for client in WR.GetDeadPlayers() do
         local jobid = client.AssignedJob.Prefab.Identifier.value
         if not WR.teamSpawnBlackList[jobid] then
