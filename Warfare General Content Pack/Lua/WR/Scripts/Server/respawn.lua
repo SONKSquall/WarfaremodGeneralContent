fd9a536edd89a0764e92286de9f019dcb0154415
SONKSquall <sonk.squall@gmail.com>
2025-06-07 15:14:56 -0500

    fix some respawn jank

--- Warfare General Content Pack/Lua/WR/Scripts/Server/respawn.lua
+++ Warfare General Content Pack/Lua/WR/Scripts/Server/respawn.lua
@@ -38,22 +38,31 @@ function WR.thinkFunctions.respawn()
         timer.time = timer.time - 1
 
         if timer.time % 1800 == 0 and timer.time > 1 then
-                if timer.time <= 1800 then
-                    for client in WR.getPlayersByJob(Client.ClientList,job) do
-                        WR.SendMessagetoClient(WR.FormatTime(timer.time/60).." before respawn.",client,WR.messagesFormats.info)
-                    end
-                else
-                    for client in WR.getPlayersByJob(WR.GetDeadPlayers(),job) do
-                        WR.SendMessagetoClient(WR.FormatTime(timer.time/60).." before respawn.",client)
-                    end
-                end
+            local message = WR.FormatTime(timer.time/60).." before respawn."
+            local format = WR.messagesFormats.default
+            local clients = WR.getPlayersByJob(WR.GetDeadPlayers(),job)
+            if timer.time <= 1800 then
+                clients = WR.getPlayersByJob(Client.ClientList,job)
+                format = WR.messagesFormats.info
+            end
+            if WR.teamSpawnBlackList[job] then
+                message = WR.FormatTime(timer.time/60).." before next cycle (no respawn)."
+            end
+            for client in clients do
+                WR.SendMessagetoClient(message,client,format)
+            end
         end
 
         if timer.time <= 0 then
             timer.time = math.floor(WR.Lerp(WR.InvLerp(WR.tick,0,WR.tickmax),30*60,WR.defaultRespawnInterval * timer.multiplier))
 
+
+            local message = "Respawning..."
+            if WR.teamSpawnBlackList[job] then
+                message = "Respawn interuped by enemy presence last cycle!"
+            end
             for client in WR.getPlayersByJob(Client.ClientList,job) do
-                WR.SendMessagetoClient("Respawning...",client,WR.messagesFormats.info)
+                WR.SendMessagetoClient(message,client,WR.messagesFormats.info)
             end
 
             WR.AssignBalanceJobs()
@@ -69,18 +78,17 @@ function WR.thinkFunctions.respawn()
                     WR.spawnHuman(client,job,spawnPoint.WorldPosition)
                 end
             end
-        end
-    end
 
-    -- teams with captured objectives can spawn once
-    local basesOccupied = 0
-    for area in WR.objectives do
-        if area.captured then basesOccupied = basesOccupied + 1 end
-        WR.teamSpawnBlackList[area.defender] = area.captured
-    end
-    -- if both areas are taken then both teams can respawn
-    if basesOccupied >= 2 then
-        WR.teamSpawnBlackList = {}
+            -- teams with captured objectives can spawn once
+            local basesOccupied = 0
+            for area in WR.objectives do
+                if area.captured then basesOccupied = basesOccupied + 1 end
+                if area.defender == job then
+                    WR.teamSpawnBlackList[job] = area.captured
+                end
+                WR.teamSpawnBlackList[job] = WR.teamSpawnBlackList[job] and basesOccupied < 2
+            end
+        end
     end
 end
 
