123382b9e3c3235689ebd1c7532de1dc937c3f8e
SONKSquall <sonk.squall@gmail.com>
2026-02-17 18:10:05 -0600

    Rework entire respawn system

--- Warfare General Content Pack/Lua/WR/Scripts/Server/respawn.lua
+++ Warfare General Content Pack/Lua/WR/Scripts/Server/respawn.lua
@@ -1,5 +1,20 @@
 if CLIENT then return end
 
+function WR.getPlayersByJob(clients,id)
+    local filteredClients = {}
+
+    for client in clients do
+        if WR.id(client,{"Character","Info","Job"}) == id or
+        not client.Character and WR.id(client,{"AssignedJob"}) == id then
+            table.insert(filteredClients,client)
+        end
+    end
+
+    return filteredClients
+end
+--[[
+WR.respawnEnabled = true
+
 WR.teamSpawnBlackList = {}
 
 WR.defaultRespawnInterval = 180*60
@@ -14,21 +29,6 @@ WR.respawns = {
         multiplier = 1
     }
 }
-
-function WR.getPlayersByJob(clients,id)
-    local filteredClients = {}
-
-    for client in clients do
-        if WR.id(client,{"Character","Info","Job"}) == id or
-        not client.Character and WR.id(client,{"AssignedJob"}) == id then
-            table.insert(filteredClients,client)
-        end
-    end
-
-    return filteredClients
-end
-
-WR.respawnEnabled = true
 
 function WR.thinkFunctions.respawn()
 
@@ -66,11 +66,6 @@ function WR.thinkFunctions.respawn()
             end
 
             WR.AssignBalanceJobs()
-            for char in Character.CharacterList do
-                if char.IsDead or not char.IsHuman then
-                    char.DespawnNow()
-                end
-            end
 
             for client in WR.getPlayersByJob(WR.GetDeadPlayers(),job) do
                 if not WR.teamSpawnBlackList[job] then
@@ -97,5 +92,105 @@ function WR.roundStartFunctions.respawn()
     for timer in WR.respawns do
         timer.time = WR.defaultRespawnInterval
         timer.multiplier = 1
+    end
+end
+]]
+WR.defaultRespawnInterval = 125*60
+
+WR.lastRespawnTime = 0
+WR.respawnTime = 0
+
+function WR.respawnClient(client,job,fliter)
+    fliter = fliter or function (c,j)
+        return true
+    end
+
+    if fliter(client,job) then
+        local spawnPoint = WR.getRandomWaypointByJob(job)
+        WR.spawnHuman(client,job,spawnPoint.WorldPosition)
+    end
+end
+
+function WR.respawnClients(clients,job,fliter)
+    for client in clients do
+        WR.respawnClient(client,job,fliter)
+    end
+end
+
+function WR.defaultRespawnFilter(client,job)
+    if client.SpectateOnly then return false end
+    if client.Character then
+        if not client.Character.IsDead then
+            return false
+        end
+    end
+    for tbl in WR.objectives do
+        if tbl.defender == WR.defender(job) then
+            WR.SendMessagetoClient("You can't respawn because there's enemies in your base!",client,WR.messagesFormats.block)
+            if tbl.captured then return false end
+        end
+    end
+    return true
+end
+
+function WR.thinkFunctions.respawn()
+    if not Game.RoundStarted or WR.Game.ending then return end
+
+    if WR.tick % 6 ~= 0 then return end
+    if WR.respawnTime > WR.tick then return end
+
+    WR.respawnTime = WR.tick + WR.defaultRespawnInterval
+    WR.lastRespawnTime = WR.tick
+    print("Time till respawn: ",WR.respawnTime)
+    print("Current time: ",WR.tick)
+
+    print("Respawning!")
+    WR.SendMessageToAllClients("Respawning!",WR.messagesFormats.info)
+
+    WR.AssignBalanceJobs()
+    for client in Client.ClientList do
+        local job = WR.id(client,{"Character","Info","Job"}) or WR.id(client,{"AssignedJob"})
+        if job then
+            WR.respawnClient(client,job,WR.defaultRespawnFilter)
+        else
+            print("Was not able to spawn: ",client, ", job was nill.")
+        end
+    end
+
+end
+
+function WR.thinkFunctions.respawnMessage()
+    if not Game.RoundStarted or WR.Game.ending then return end
+
+    if WR.respawnTime - WR.tick - 60*60 == 0 then
+        WR.SendMessageToAllClients("1 minute till respawn.",WR.messagesFormats.info)
+    end
+end
+
+
+function WR.thinkFunctions.despawnMonstersAndCorpses()
+    if WR.tick % (60*60) ~= 0 then return end
+
+    print("Removing corpses & monsters...")
+    for char in Character.CharacterList do
+        if not char.Removed and (char.IsDead or not char.IsHuman) then
+            print("Removed: ",char)
+            char.DespawnNow()
+        end
+    end
+end
+
+function WR.characterDeathFunctions.respawnSoonAfterDeath(char)
+    if not char.IsHuman then return end
+    local client = Util.FindClientCharacter(char)
+    if client then
+        if WR.tick - WR.lastRespawnTime < (WR.defaultRespawnInterval*0.2) then -- within 20% of the respawn interval
+            local job = WR.id(client,{"Character","Info","Job"})
+            if job then
+                WR.respawnClient(client,job,WR.defaultRespawnFilter)
+            else
+                print("Was not able to spawn: ",client, ", job was nill.")
+            end
+        end
     end
 end
